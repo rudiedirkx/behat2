e79f2b1615ff531b218e71f25cfe3b5163722ba7
Rudie Dirkx <github@hotblocks.nl>
2016-03-26 01:56:16 +0100

    +alinears ft

---
+++ features/alinears.feature
@@ -0,0 +1,42 @@
+Feature: Alinears
+	Do some highschool maths
+
+	Scenario: Positive power
+		Given we start with "2"
+		When we "8" power it
+		Then we have "256"
+
+	Scenario: Negative power
+		Given we start with "5"
+		When we "-1" power it
+		Then we have "0.2"
+
+	Scenario: More negative power
+		Given we start with "2"
+		When we "-2" power it
+		Then we have "0.25"
+
+	Scenario: Negative base
+		Given we start with "-2"
+		When we "3" power it
+		Then we have "-8"
+
+	Scenario: Positive root
+		Given we start with "9"
+		When we "2" root it
+		Then we have "3"
+
+	Scenario: Small root
+		Given we start with "5"
+		When we "0.5" root it
+		Then we have "25"
+
+	Scenario: Negative root
+		Given we start with "5"
+		When we "-1" root it
+		Then we have "0.2"
+
+	Scenario: More negative root
+		Given we start with "9"
+		When we "-2" root it
+		Then we have "1/3"
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -58,23 +58,47 @@ class FeatureContext implements Context, SnippetAcceptingContext {
 		$this->amount /= (float) $amount;
 	}
 
-
+	/**
+	 * @When we :exponent power it
+	 */
+	public function wePowerIt($exponent) {
+		$this->amount = pow($this->amount, (float) $exponent);
+	}
 
 	/**
-	 * @Then we have :amount
+	 * @When we :exponent root it
 	 */
-	public function weHave($amount) {
-		$this->assertAmount($amount);
+	public function weRootIt($exponent) {
+		$this->amount = pow($this->amount, 1 / (float) $exponent);
 	}
 
 
 
 	/**
+	 * @Then we have :amount
 	 *
+	 * The amount to check against can be a fraction, e.g. "1/3"
 	 */
-	protected function assertAmount($amount) {
-		if ((float) $amount != $this->amount) {
-			throw new \Exception("Result amount should be $amount, but it is {$this->amount}");
+	public function weHave($amount) {
+		$check = (float) $amount;
+		if (preg_match('#^(\d+)/(\d+)$#', $amount, $match)) {
+			$check = (float) $match[1] / (float) $match[2];
+		}
+
+		$this->assertAmount($check, $amount);
+	}
+
+
+
+	/**
+	 * Assert result amount.
+	 */
+	protected function assertAmount($amount, $amountLabel = null) {
+		$precision = (ini_get('precision') ?: 14) / 2;
+
+		if (round((float) $amount, $precision) != round($this->amount, $precision)) {
+			$amountLabel or $amountLabel = $amount;
+			throw new \Exception("Result amount should be $amountLabel, but it is {$this->amount}");
 		}
 	}
 
